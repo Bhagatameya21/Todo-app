7134535339fbd90a11e16b5dbc8480c97a1fc6de
Ameya Bhagat <Bhagatameya21@gmail.com>
2023-02-02 16:19:22 +0530

    added CRUD Routes and Final Touch

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const port=process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
 
-require('dotenv').config()
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
-app.use("/static",express.static("public"));
+app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-
 //connection to db
-
-mongoose.set("useFindAndModify", false);
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () => {
-console.log("Connected to db!");
-app.listen(port,()=>console.log(`Server up and running at ${port}`));
-})
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("Connected to db!");
+  app.listen(PORT, () => console.log(`Server up and running at ${PORT}`));
+});
 
 //view engine configuration
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 //GET
-app.get('/',(req,res)=>{
-    res.render('todo.ejs');
-})
+app.get("/", (req, res) => {
+  TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+  });
+});
 
 //POST
-app.post('/',(req, res) => {
-    console.log(req.body);
-    });
+app.post("/", async (req, res) => {
+  const todoTask = new TodoTask({
+    content: req.body.content,
+  });
+  try {
+    await todoTask.save();
+    res.redirect("/");
+  } catch (err) {
+    res.redirect("/");
+  }
+});
 
-app.listen();
+//UPDATE
+app
+.route("/edit/:id")
+.get((req, res) => {
+const id = req.params.id;
+TodoTask.find({}, (err, tasks) => {
+res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+});
+})
+.post((req, res) => {
+const id = req.params.id;
+TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+if (err) return res.send(500, err);
+res.redirect("/");
+});
+});
 
-
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, err => {
+  if (err) return res.send(500, err);
+  res.redirect("/");
+  });
+  });
